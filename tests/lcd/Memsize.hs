6fd24012a5d0cdfc223ad5bee53870df2751b365
Nikk0o <nknk@lagartao.gay>
2025-06-30 02:03:13 -0300

    changed Memsize.hs

--- tests/lcd/Memsize.hs
+++ tests/lcd/Memsize.hs
@@ -1,3 +1,3 @@
 import System.IO
 
-main = readFile "inst_mem.hex" >>= putStrLn . show . (`sub` 1) . length . words where sub a b = a - b
+main = readFile "inst_mem.hex" >>= putStrLn . show . (\l -> l - 1) . length . words
